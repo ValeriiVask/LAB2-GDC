d7136a3cb46f226c3b56ab3d579eeefd3d5b9a6f
Valerii <vaskiva7807_aehit@students.vizja.pl>
2024-10-29 16:06:25 +0100

    Initial commit

---
+++ GCDLib/GCDLib.cpp
@@ -0,0 +1,8 @@
+unsigned long long GCD_modulo(unsigned long long a, unsigned long long b) {
+    while (b != 0) {
+        unsigned long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
